9692f729c7f9c33fe39aa5978d52d0201b48305f
Deividi Santos <deividifirenze@gmail.com>
2026-04-22 16:54:27 -0300

    inicio bind

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('pokedex');
+  pesquisa = "";
 }
