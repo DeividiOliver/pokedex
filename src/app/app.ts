d055d458d75ac377e3c66e350427ed72b149046e
Deividi Santos <deividifirenze@gmail.com>
2026-05-06 13:13:34 -0300

    pokedex alteração
    Co-authored-by: Gabriel da Silva Rodrigues <grodrigues.ti@outlook.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,22 +2,26 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { HttpService } from './http.service';
+import { CommonModule } from '@angular/common';
+import { Card } from './components/card/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, Card],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App implements OnInit{
   pesquisa = "teste"
   
   service = inject(HttpService)
-  
+
+  lista: any = [];
+
   ngOnInit(): void {
     
       this.service.obterPokemon().subscribe(resposta => {
-         console.log(resposta);
+         this.lista = resposta.results;
   })
 ;
 }}
