b903fdd2d47cba821ff6f15763f42519a45de9cc
Deividi Santos <deividifirenze@gmail.com>
2026-05-12 21:56:07 -0300

    ajuste card html e ts

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,19 +1,39 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common'; // IMPORTANTE para o ngClass
+import { CommonModule } from '@angular/common';
+
+// Esta interface define a estrutura exata do Pokémon que vem da API
+// Isso é o TypeScript Rigoroso: o VS Code vai te avisar se você digitar algo errado
+export interface PokemonData {
+  id: number;
+  name: string;
+  types: { type: { name: string } }[];
+  sprites: {
+    other: {
+      'official-artwork': {
+        front_default: string;
+      };
+    };
+  };
+}
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule], // Adicione o CommonModule aqui
+  imports: [CommonModule],
   templateUrl: './card.html',
   styleUrl: './card.scss'
 })
 export class CardComponent {
-  // Verifique se o nome das variáveis bate com o que usamos no HTML
-  @Input() id: number = 0;
-  @Input() nome: string = '';
-  
-  getPokemonImage(id: number): string {
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+  // O símbolo '!' diz ao TS que este valor será preenchido pelo componente pai
+  @Input() pokemon!: PokemonData; 
+
+  // Getter para facilitar o acesso à imagem no HTML
+  get pokemonImage(): string {
+    return this.pokemon.sprites.other['official-artwork'].front_default;
+  }
+
+  // Getter para pegar o tipo principal e definir a cor do card
+  get mainType(): string {
+    return this.pokemon.types[0].type.name;
   }
 }
